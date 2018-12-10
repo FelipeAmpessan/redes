1315d49594a7ee9c758ba25beb3ac805eed1a472
Felipe <felipe.younner@gmail.com>
2018-12-10 14:52:19 -0200

    ort

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
+  socket.on('value change', function(msg){
+      io.emit('value change', msg);
+  });)
 });
 
 http.listen(port, function(){
